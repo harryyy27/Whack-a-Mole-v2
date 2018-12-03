19cc056317b6d58ea8d401a65c295c4d29cf00fc
Armand-Lluka <armandwork@hotmail.co.uk>
2018-12-03 13:44:17 +0000

    file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import styled from "styled-components"
+import Header from "./Header"
+
+
+
+const Button = styled.button.attrs({
+  className: "f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-near-black center"
+})`
+background: red;
+`
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Whack'a'mole</h1>
+      <Header></Header>
+        <Button>Press me</Button>
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from "styled-components"
+
+const Logo = styled.h1.attrs({
+    className: "blue tc"
+  })``
+  
+
+ export default () => (
+        <Logo>Whack'a'mole</Logo>
+ );
+   
+        
