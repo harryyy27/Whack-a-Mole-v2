8ba6b0d8a20dd2c28df872304a2883d9fe2efbad
Armand-Lluka <armandwork@hotmail.co.uk>
2018-12-03 15:15:41 +0000

    Smug face getting thumped

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
-import Styled from "styled-components"
-import punch from "../images/punch.png"
-import trump from "../images/Trump.png"
+import React, { Component } from "react";
+import Styled from "styled-components";
+import punch from "../images/punch.png";
+import trump from "../images/Trump.png";
 
 const Title = Styled.h1.attrs({
-    className: " Roboto  relative top-1 "
+  className: " Roboto  relative top-1 "
 })`
 @import url(https://fonts.googleapis.com/css?family=Roboto);
 font-family: Roboto
 `;
 
 const Image = Styled.img.attrs({
-    className: "h4 dib relative  left-1"
+  className: "h4 dib relative  left-1"
 })``;
 
 const ImageFlip = Styled.img.attrs({
-    className: "h4 dib relative  right-1 "
+  className: "h4 dib relative  right-1 "
 })`
 transform: scaleX(-1)
 `;
 
-const Container = Styled.header.attrs({
-    className: "tc bg-light-gray"
-})``
+const Trump = Styled.img.attrs({
+  className: "br-100  h4 w4 dib"
+})``;
 
+const Container = Styled.header.attrs({
+  className: "tc bg-light-gray"
+})``;
 
 export default class Landing extends Component {
-    render() {
-        return (
-            <div>
-                <Container>
-                   
-                   <Title>Thump 'a' Trump</Title>
-                    <ImageFlip src={punch} />
-                                        <img
-                                            src={trump}
-                                            className="br-100  h4 w4 dib " alt="avatar" />
-
-                    <Image src={punch} />
-
-                   
-                </Container>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Container>
+          <Title>Thump 'a' Trump</Title>
+          <ImageFlip src={punch} />
+          <Trump src={trump} alt="avatar" />
+          <Image src={punch} />
+        </Container>
+      </div>
+    );
+  }
 }
 
-// export default Landing; 
+// export default Landing;
