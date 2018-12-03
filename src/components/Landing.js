60dead4396aa5b1e0c21ca9670ac48d8f75dba41
Armand-Lluka <armandwork@hotmail.co.uk>
2018-12-03 15:07:41 +0000

    image of trump smirking + gray background

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Styled from "styled-components"
 import punch from "../images/punch.png"
-
+import trump from "../images/Trump.png"
 
 const Title = Styled.h1.attrs({
     className: " raleway dib relative bottom-2 "
@@ -11,29 +11,38 @@ font-family: Raleway
 `;
 
 const Image = Styled.img.attrs({
-    className: "h4 dib relative top-1"
+    className: "h4 dib relative top-1 left-1"
 })``;
 
 const ImageFlip = Styled.img.attrs({
-    className: "h4 dib relative top-1  "
+    className: "h4 dib relative top-1 right-1 "
 })`
 transform: scaleX(-1)
 `;
 
 const Container = Styled.header.attrs({
-    className: "tc flex-auto"
+    className: "tc bg-light-gray"
 })``
 
 
 export default class Landing extends Component {
     render() {
         return (
-            <Container>
-                <ImageFlip src={punch}  />
-                <Title>Thump 'a' Trump</Title>
-                <Image src={punch} />
-            </Container>
+            <div>
+                <Container>
+                    <ImageFlip src={punch} />
+                    <Title>Thump 'a' Trump</Title>
+                    <Image src={punch} />
 
+                    <div className="relative bottom-2 ">
+                                        <img
+                                            src={trump}
+                                            className="br-100  h4 w4 dib " alt="avatar" />
+
+                                    </div>
+                </Container>
+                
+            </div>
         )
     }
 }
